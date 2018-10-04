d19cc013715e71ce27f4ddd5b8b8157c267d3378
Daniel Schoch <d.schoch90@gmail.com>
2018-10-04 03:43:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,6 +42,12 @@ class Student
 
   end
 
+  def self.create (name, grade)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
+
   def self.new_from_db(row)
     new_student = self.new(row[1], row[2], row[0])
     new_student
